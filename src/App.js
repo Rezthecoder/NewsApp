48a7348dbf1b2f027fee2fffee51df1c6dd5f3c7
Suresh <suresh@gmail.com>
2022-10-29 12:49:35 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  name = "Suresh"
+  render() {
+    return (
+      <div>Hello World {this.name}</div>
+    )
+  }
+}
